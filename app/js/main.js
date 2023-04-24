5bca159e74f39eef561bcf49243865ed90e9600c
david-dya4yk <daviddya4yk@mail.ru>
2023-04-24 23:45:12 +0300

    lesson 3

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,64 @@
-const title = 'javaScript';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 42;
-const rollBack = 55;
-const fullPrice = 1000;
-const adaptive = true;
+"use strict";
+// const title = prompt('как называется ваш проект?');
+// const screens = prompt('Какие типы экранов нужно разработать?','Простые, Сложные, Интерактивные');
+// const screenPrice = prompt("Сколько будет стоить данная работа?", "1200");
+// const rollBack = 55;
+// const adaptive = confirm('Нужен ли адаптив на сайте?');
+// const service1 = prompt('Какой дополнительный тип услуги нужен?');
+// const servicePrice1 = prompt("Сколько это будет стоить?");
+// const service2 = prompt('Какой дополнительный тип услуги нужен?');
+// const servicePrice2 = prompt("Сколько это будет стоить?");
 
-const splits = screens.split(',');
-const workerPrice = fullPrice * rollBack / 100;
+// const splits = screens.split(',');
+// const fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+//   if (fullPrice >= 30000) {
+//     alert("Даем скидку в 10%");
+//   } else if (fullPrice >= 15000) {
+//     alert("Даем скидку в 5%");
+//   } else if (fullPrice > 0) {
+//     alert('Скидка не предусмотрена')
+//   } else if (fullPrice <= 0) {
+//     alert("Что то пошло не так");
+//   }
 
+// const workerPrice = (fullPrice * rollBack) / 100;
+// const servicePercentPrice = Math.round(fullPrice - workerPrice);
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-console.log(screens.length);
-console.log(fullPrice + 'UAH');
-console.log(screenPrice + 'UAH');
-console.log(screens.toLowerCase());
-console.log(splits);
-console.log(workerPrice)
+// console.log('title',title);
+// console.log('fullPrice',fullPrice);
+// console.log('adaptive',adaptive);
+// console.log('screens',screens.length);
+// console.log('fullPrice', fullPrice + 'UAH');
+// console.log('screenPrice', screenPrice + 'UAH');
+// console.log('screens',screens.toLowerCase());
+// console.log('splits', splits);
+// console.log('workerPrice', workerPrice)
+// console.log('servicePercentPrice', servicePercentPrice);
 
+// let num = 266219;
+// const numToString = Array.from(String(num)).map((i) => Number(i));
+// const multiplyNum = numToString.reduce((acc, rec) => acc * rec);
+// const degreeMultiplyNum = multiplyNum ** 3;
+// const sliceDegreeMultiplyNum = String(num).slice(0, 2);
 
-let num = 266219;
-const numToString = Array.from(String(num)).map(i=>Number(i));
-const multiplyNum = numToString.reduce((acc, rec) => acc * rec);
-const degreeMultiplyNum = multiplyNum ** 3;
-const sliceDegreeMultiplyNum = String(num).slice(0,2);
-console.log(sliceDegreeMultiplyNum)
+// const a = 10;
+// console.log(a)
 
+const lang = prompt("Выберите язык", "ru & en");
+const weekDaysRu = ["Понедельник", " Вторник", "Среда", "Четверг", "П'ятница"];
+const weekDaysEn = ["Monday", "Tuesday", "Wednesday", "Thusday", "Friday"];
+
+// if (lang == 'ru') {
+//   console.log(weekDaysRu);
+// } else if (lang == 'en'){
+//   console.log(weekDaysEn)
+// }
+
+switch (lang) {
+  case "ru":
+    console.log(weekDaysRu);
+    break
+  case "en":
+    console.log(weekDaysEn);
+    break
+}
