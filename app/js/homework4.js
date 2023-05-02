6ea67d915b0ffc013d99c47c47866025713fdbe8
david-dya4yk <daviddya4yk@mail.ru>
2023-05-02 20:20:52 +0300

    done homework 5

--- app/js/homework4.js
+++ app/js/homework4.js
@@ -1,19 +1,87 @@
-"use strict"
+"use strict";
 
-const text = prompt('Введите текст');
+const text = prompt("Введите текст", "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum repellendus impedit soluta est ad, ea, nihil quas vitae qu");
 // const text = 5432;
 
 const checkToString = function (str) {
-    if (typeof str === 'string'){
-        str.trim()
-        if (str.length <= 10){
-           return str
-        } else {
-           return  str.slice(0, 10) + "...."
-        }
-    } else {
-        return 'тип не строка'
-    }
-}
+  if (typeof str !== "string") {
+    return "тип не строка";
+  }
 
-console.log('text', checkToString(text))
+  const trimmedString = str.trim();
+
+  if (trimmedString.length <= 30) {
+    return trimmedString;
+  }
+  return trimmedString.slice(0, 30) + "...";
+};
+
+console.log("text", checkToString(text));
+
+// const text = prompt(
+//     "Введите текст",
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto debitis reprehenderit itaque"
+//   );
+// const text = 5432;
+//   const MAX_STRING_LENGTH = 30;
+//   const SEPARATOR = "...";
+
+//   const checkToString = function (str) {
+//     str = str.trim();
+
+//     if (typeof str !== "string") {
+//       return "тип не строка";
+//     }
+
+//     return str.length <= MAX_STRING_LENGTH
+//       ? str
+//       : str.slice(0, MAX_STRING_LENGTH) + SEPARATOR;
+//   };
+
+//   console.log("result: ", checkToString(text));
+
+// ===========================
+
+// const checkToString = function (str) {
+//   str = str.trim();
+//   if (typeof str !== "string") {
+//     return "тип не строка";
+//   }
+
+//   return str.length <= 30 ? str : str.slice(0, 30) + "....";
+// };
+
+// checkToString("     asd");
+
+// ===========================
+
+// const checkToString = function (str) {
+//   if (typeof str !== "string") {
+//     return "тип не строка";
+//   }
+
+//   const trimmedString = str.trim();
+
+//   return trimmedString.length <= 30
+//     ? trimmedString
+//     : trimmedString.slice(0, 30) + "....";
+// };
+
+// console.log("text", checkToString(text));
+
+// ===========================
+
+// const checkToString = function (str) {
+//   if (typeof str !== "string") {
+//     return "тип не строка";
+//   }
+
+//   const trimmedString = str.trim();
+//   if (trimmedString.length <= 30) {
+//     return trimmedString;
+//   }
+
+//   return trimmedString.slice(0, 30) + "....";
+// };
+
+// console.log("text", checkToString(text));
--- app/js/main.js
+++ app/js/main.js
@@ -1,51 +1,112 @@
 "use strict";
 
-const title = prompt('Как называется ваш проект?');
-const screens = prompt('Какие типы екранов нужно розрабативать?');
-const screenPrice = +prompt('Сколько будет стоить даная работа?');
-const adaptive = confirm('Нужен адаптив на сайте?');
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-const servicePrice2 = +prompt('Сколько это будет стоить?');
 const rollBack = 10;
-const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let allServicesPrices;
+let fullPrice;
+let servicesPercentPrice;
+let service1;
+let service2;
 
 
-const showTypeOf = function(variable) {
-    console.log(variable, typeof variable);
-}
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?", "Калькулятор верстки");
+  screens = prompt(
+    "Какие типы екранов нужно розрабативать?",
+    "Простые, Сложные"
+  );
+
+
+  do {
+    screenPrice = +prompt("Сколько будет стоить даная работа?", "    15000   ").trim();
+  } while (!isNumber(screenPrice));
+  
+
+  adaptive = confirm("Нужен адаптив на сайте?");
+};
+
+const getAllServicesPrices = function () {
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt(
+        "Какой дополнительный тип услуги нужен?",
+        "Отправка форм"
+      );
+    } else {
+      service2 = prompt(
+        "Какой дополнительный тип услуги нужен?",
+        "Отправка форм2"
+      );
+    }
+
+    let servicePrice;
+
+    do {
+      servicePrice = prompt("Сколько это будет стоить?", 7500);
+    } while (!isNumber(servicePrice));
+
+    sum += +servicePrice;
+
+  }
+  return sum
+};
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+const getFullPrice = function () {
+  return +screenPrice + +allServicesPrices;
+};
+
+const getServicesPercentPrice = function () {
+  return fullPrice - (fullPrice * (rollBack / 100));
+};
+
+const getTitle = function () {
+  return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
+};
 
 const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return "Даем скидку в 10%"
-    } else if (price >= 15000 && price < 30000){
-        return "Даем скидку в 5%"
-    } else if (price >= 0 && price < 15000){
-        return "Скидка не предусмотрена"
-    } else {
-        return "Что то пошло не так"
-    }
-}
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  }
+  if (price >= 15000) {
+    return "Даем скидку в 5%";
+  }
+  if (price >= 0) {
+    return "Скидка не предусмотрена";
+  }
 
-const getServicePercentPrices = function () {
-    return fullPrice - (fullPrice * (rollBack / 100));
-  };
+  return "Что то пошло не так";
+};
 
-const servicePercentPrice = getServicePercentPrices();
+asking();
+allServicesPrices = getAllServicesPrices();
+fullPrice = getFullPrice();
+servicesPercentPrice = getServicesPercentPrice();
+title = getTitle();
 
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log(getRollbackMessage(fullPrice));
-console.log(typeof title);
-console.log(typeof screenPrice);
-console.log(typeof adaptive);
+console.log("allServicesPrices: ", allServicesPrices);
+console.log("fullPrice: ", getRollbackMessage(fullPrice));
+console.log("title: ",  title);
+console.log("screenPrice: ",  screenPrice);
+console.log("adaptive: ",  adaptive);
 
-console.log(screens.length);
-console.log(servicePercentPrice);
+console.log("screens", screens.length);
+console.log("servicesPercentPrice", servicesPercentPrice);
 
-console.log("Стоимость верстки экранов" + " " + screenPrice + "рублей");
-
-console.log(servicePercentPrice)
+console.log("Стоимость верстки" + " " + servicesPercentPrice + "рублей");
